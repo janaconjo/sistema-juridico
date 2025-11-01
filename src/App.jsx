6eac894d642b297c57eb875123034b5c7fb32c18
Jana Conjo <JanaConjoo@gmail.com>
2025-11-01 19:24:23 +0200

    v15

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
+import Home from './Pages/Home'; // Este deve ser o seu componente da página inicial (Header, Hero, Seções)
 import AgendarAtendimento from './Pages/AgendarAtendimento';
 import Materiais from './Pages/Materiais';
-import Cadastro from './Pages/cadastro';
+import Cadastro from './Pages/cadastro'; // Este deve ser o seu componente AuthForm (o formulário de Login/Registo)
 import Advogado from './Pages/Advogado';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Rotas principais */}
+        {/*
+          ROTA PRINCIPAL: Deve carregar a página inicial completa.
+          Se o formulário de Cadastro estiver aparecendo aqui,
+          o problema está DENTRO do componente Home (Pages/Home.jsx)
+          que deve estar importando e renderizando o componente Cadastro/AuthForm.
+        */}
         <Route path="/" element={<Home />} />
 
+        {/* ROTAS SECUNDÁRIAS */}
         <Route path="/Agendaratendimento" element={<AgendarAtendimento />} />
         <Route path="/Materiais" element={<Materiais />} />
+        
+        {/* ROTA DE CADASTRO/LOGIN: Carrega o AuthForm */}
         <Route path="/Cadastro" element={<Cadastro />} />
 
         {/* Rota para o layout do Advogado */}
